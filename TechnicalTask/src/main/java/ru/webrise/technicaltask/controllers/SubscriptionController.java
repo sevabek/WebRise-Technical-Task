3a4f9842e93f0f6bd33534884011fa8f1f7c9039
Seva <wefwfe59@inbox.ru>
2025-06-09 12:30:17 +0300

    1

--- TechnicalTask/src/main/java/ru/webrise/technicaltask/controllers/SubscriptionController.java
+++ TechnicalTask/src/main/java/ru/webrise/technicaltask/controllers/SubscriptionController.java
@@ -30,7 +30,7 @@ public class SubscriptionController {
     private final BindingResultErrorHandler bindingResultErrorHandler;
 
     /**
-     * Добавляет новую подписку для указанного пользователя.
+     * Добавляет новую подписку для указанного1 пользователя.
      *
      * @param userId Идентификатор пользователя
      * @param subscriptionDTO DTO с данными новой подписки
